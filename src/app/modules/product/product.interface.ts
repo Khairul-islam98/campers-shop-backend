a9abcfde252f5c5c2406faa594e437617035eae4
khairul-islam98 <janeyrahman900@gmail.com>
2024-07-09 01:08:27 +0600

    create product model

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  name: string;
+  price: number;
+  stock: number;
+  quantity: number;
+  description: string;
+  category: string;
+  rating: number;
+  image: string;
+}
