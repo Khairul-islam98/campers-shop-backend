3cc7332003dae435018e517db7e07c166e7fd0d8
khairul-islam98 <janeyrahman900@gmail.com>
2024-07-09 00:49:49 +0600

    create routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+const moduleRoutes = [{}];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
