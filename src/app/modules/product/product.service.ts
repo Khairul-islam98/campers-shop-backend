a3bc3ef3319e772b91b38c6010ce821eb93878f2
khairul-islam98 <janeyrahman900@gmail.com>
2024-07-10 13:30:24 +0600

    create service all product single and update delete

---
+++ src/app/modules/product/product.service.ts
@@ -0,0 +1,44 @@
+import QueryBuilder from '../../builder/QueryBuilder';
+import { productSeachableFields } from './product.constant';
+import { IProduct } from './product.interface';
+import { Product } from './product.model';
+
+const createProductIntoDB = async (payload: IProduct) => {
+  const product = await Product.create(payload);
+  return product;
+};
+
+const getAllProductFromDB = async (query: Record<string, unknown>) => {
+  const productQuery = new QueryBuilder(Product.find(), query)
+    .search(productSeachableFields)
+    .filter()
+    .sort();
+  const result = await productQuery.modelQuery;
+  return result;
+};
+
+const getSingleProductFromDB = async (id: string) => {
+  const result = await Product.findById(id);
+  return result;
+};
+const updateProductIntoDB = async (id: string, payload: IProduct) => {
+  const result = await Product.findByIdAndUpdate(
+    id,
+    { $set: payload },
+    { new: true },
+  );
+  return result;
+};
+
+const deleteProductFromDB = async (id: string) => {
+  const result = await Product.findByIdAndDelete(id);
+  return result;
+};
+
+export const ProductServices = {
+  createProductIntoDB,
+  getAllProductFromDB,
+  getSingleProductFromDB,
+  updateProductIntoDB,
+  deleteProductFromDB,
+};
